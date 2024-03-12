1dcab2cfa9237e30a9682154928dbbc2de2fb144
Polese-e <enzopolesedamasceno@gmail.com>
2024-03-11 21:38:23 -0300

    Update - In-Depth Study of the filter Method
    This commit brings an in-depth study of the filter method in arrays, exploring concepts and specific applications in the Advanced Arrays section. Annotations have been adjusted to highlight the effective use of the filter method in different advanced scenarios. This is another step in the exploration of advanced array methods.

---
+++ aula65/aula65.js
@@ -0,0 +1,33 @@
+/* O método filter() em JavaScript é utilizado em arrays para criar um novo 
+array contendo apenas os elementos que atendem a uma determinada condição, 
+especificada por uma função de retorno de chamada (callback). A função de 
+callback deve retornar true para os elementos que devem ser incluídos no novo 
+array e false para os elementos que devem ser excluídos. */
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+
+const numerosFiltrados = numeros.filter((valor) => valor > 10);
+
+console.log(numerosFiltrados);
+console.log("================================================================");
+
+/* ========================================================================== */
+
+const pessoas = [
+  { nome: "Enzo", idade: 19 },
+  { nome: "Alícya", idade: 18 },
+  { nome: "Igor", idade: 20 },
+  { nome: "Gabriel", idade: 22 },
+  { nome: "Gustavo", idade: 23 },
+  { nome: "João", idade: 26 },
+  { nome: "Pedro", idade: 30 },
+];
+const pessoasComNomeGrande = pessoas.filter((obj) => obj.nome.length >= 5);
+const pessoasNovas = pessoas.filter((obj) => obj.idade <= 20);
+const nomeTerminaComO = pessoas.filter((obj) =>
+  obj.nome.toLowerCase().endsWith("o")
+);
+
+console.log(pessoasComNomeGrande);
+console.log(pessoasNovas);
+console.log(nomeTerminaComO);
