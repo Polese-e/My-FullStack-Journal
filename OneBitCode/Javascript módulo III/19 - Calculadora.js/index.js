77444ad987b152f92ade1bbf9e1505573d6e38c7
Polese-e <enzopolesedamasceno@gmail.com>
2024-04-03 20:06:38 -0300

    Guided Practical Project - Building a Calculator with HTML, CSS, and JavaScript - Part 1 at One Bit Code
    This commit presents Part 1 of a guided practical study on how to build a calculator using HTML, CSS, and JavaScript as part of the One Bit Code course. The provided code contains the basic structure of the project, including HTML elements for data input, mathematical operation buttons, and CSS styles for the interface. Additionally, basic functionalities for manipulating input data and calculating mathematical operations are implemented.

---
+++ OneBitCode/Javascript módulo III/19 - Calculadora.js/index.js
@@ -0,0 +1,59 @@
+const main = document.querySelector("main");
+const root = document.querySelector(":root");
+const input = document.getElementById("input");
+const resultInput = document.getElementById("result");
+
+const allowedKeys = [
+  "(",
+  ")",
+  "/",
+  "*",
+  "-",
+  "+",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+  "1",
+  "0",
+  ".",
+  "%",
+  " ",
+];
+
+document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
+  charKeyBtn.addEventListener("click", function () {
+    const value = charKeyBtn.dataset.value;
+    input.value += value;
+  });
+});
+
+document.getElementById("clear").addEventListener("click", function () {
+  input.value = "";
+  input.focus();
+});
+
+input.addEventListener("keydown", function (ev) {
+  ev.preventDefault();
+  if (allowedKeys.includes(ev.key)) {
+    input.value += ev.key;
+    return;
+  }
+  if (ev.key === "Backspace") {
+    input.value = input.value.slice(0, -1);
+  }
+  if (ev.key === "Enter") {
+    calculate();
+  }
+});
+
+document.getElementById("equal").addEventListener("click", calculate);
+
+function calculate() {
+  const result = eval(input.value);
+  resultInput.value = result;
+}
