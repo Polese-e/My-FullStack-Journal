aea20ed90afaa5840d98b1db74d92ef7131e388c
Polese-e <enzopolesedamasceno@gmail.com>
2024-04-04 17:23:36 -0300

    Study on Guided Practical Project - Creating a Calculator with HTML, CSS, and JavaScript - Part 2 at One Bit Code
    In this commit, the updates in Part 2 of the guided practical project on creating a calculator using HTML, CSS, and JavaScript, as part of the One Bit Code course, are presented. The main changes include adding functionality to copy the result to the clipboard, implementing visual feedback when copying the result, updating the theme switch button to adjust the CSS color variables according to the selected theme, fixing a bug that displayed "ERROR" in the result even in successful calculation cases, and improving the style and layout of the project to provide a more pleasant and intuitive user experience.

--- OneBitCode/Javascript módulo III/19 - Calculadora.js/index.js
+++ OneBitCode/Javascript módulo III/19 - Calculadora.js/index.js
@@ -54,6 +54,39 @@ input.addEventListener("keydown", function (ev) {
 document.getElementById("equal").addEventListener("click", calculate);
 
 function calculate() {
+  resultInput.value = "ERROR";
+  resultInput.classList.add("error");
   const result = eval(input.value);
   resultInput.value = result;
+  resultInput.classList.remove("error");
 }
+
+document
+  .getElementById("copyToClipboard")
+  .addEventListener("click", function (ev) {
+    const button = ev.currentTarget;
+    if (button.innerText === "Copy") {
+      button.innerText = "Copied!";
+      button.classList.add("success");
+      navigator.clipboard.writeText(resultInput.value);
+    } else {
+      button.innerText = "Copy";
+      button.classList.remove("success");
+    }
+  });
+
+document.getElementById("themeSwitcher").addEventListener("click", function () {
+  if (main.dataset.theme === "dark") {
+    root.style.setProperty("--bg-color", "#f1f5f9");
+    root.style.setProperty("--border-color", "#aaa");
+    root.style.setProperty("--font-color", "#212529");
+    root.style.setProperty("--primary-color", "#26834a");
+    main.dataset.theme = "light";
+  } else {
+    root.style.setProperty("--bg-color", "#212529");
+    root.style.setProperty("--border-color", "#666");
+    root.style.setProperty("--font-color", "#f1f5f9");
+    root.style.setProperty("--primary-color", "#4dff91");
+    main.dataset.theme = "dark";
+  }
+});
