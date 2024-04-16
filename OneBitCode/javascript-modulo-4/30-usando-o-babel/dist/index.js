5a7f85ccf0da19a533714f861276a4eed4c69c29
Polese-e <enzopolesedamasceno@gmail.com>
2024-04-16 18:24:27 -0300

    Introducing Babel for JavaScript Transpilation - One Bit Code
    This commit introduces the setup and usage of Babel for JavaScript transpilation. Initially, the required Babel packages (@babel/core, @babel/cli, @babel/preset-env) are installed as development dependencies using the command "npm install --save-dev @babel/core @babel/cli @babel/preset-env". Additionally, the babel.config.js file is created to configure Babel presets, specifically using "@babel/preset-env". Inside package.json, a script named "babel" is defined to execute Babel on the src directory and output the transpiled code to the dist directory. The preset for Babel is specified as "@babel/preset-env". Finally, the process of using Babel is briefly summarized.

---
+++ OneBitCode/javascript-modulo-4/30-usando-o-babel/dist/index.js
@@ -0,0 +1,76 @@
+"use strict";
+
+// Função da Média Simples
+var average = function average() {
+  for (var _len = arguments.length, numbers = new Array(_len), _key = 0; _key < _len; _key++) {
+    numbers[_key] = arguments[_key];
+  }
+  var sum = numbers.reduce(function (accum, num) {
+    return accum + num;
+  }, 0);
+  return sum / numbers.length;
+};
+console.log("M\xE9dia Aritm\xE9tica Simples: ".concat(average(3, 6, 10, 9)));
+
+// Função da Média Ponderada
+var weightedAverage = function weightedAverage() {
+  for (var _len2 = arguments.length, entries = new Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+    entries[_key2] = arguments[_key2];
+  }
+  var sum = entries.reduce(function (accum, _ref) {
+    var number = _ref.number,
+      weight = _ref.weight;
+    return accum + number * (weight !== null && weight !== void 0 ? weight : 1);
+  }, 0);
+  var weightSum = entries.reduce(function (accum, entry) {
+    var _entry$weight;
+    return accum + ((_entry$weight = entry.weight) !== null && _entry$weight !== void 0 ? _entry$weight : 1);
+  }, 0);
+  return sum / weightSum;
+};
+console.log("M\xE9dia Ponderada: ".concat(weightedAverage({
+  number: 9,
+  weight: 3
+}, {
+  number: 7
+}, {
+  number: 10,
+  weight: 1
+})));
+
+// Função da Mediana
+var median = function median() {
+  for (var _len3 = arguments.length, numbers = new Array(_len3), _key3 = 0; _key3 < _len3; _key3++) {
+    numbers[_key3] = arguments[_key3];
+  }
+  var orderedNumbers = [].concat(numbers).sort(function (a, b) {
+    return a - b;
+  });
+  var middle = Math.floor(orderedNumbers.length / 2);
+  if (orderedNumbers.length % 2 !== 0) {
+    return orderedNumbers[middle];
+  }
+  var firstMedian = orderedNumbers[middle - 1];
+  var secondMedian = orderedNumbers[middle];
+  return average(firstMedian, secondMedian);
+};
+console.log("Mediana: ".concat(median(2, 5, 99, 4, 42, 7)));
+console.log("Mediana: ".concat(median(15, 14, 8, 7, 3)));
+
+// Função da Moda
+var mode = function mode() {
+  for (var _len4 = arguments.length, numbers = new Array(_len4), _key4 = 0; _key4 < _len4; _key4++) {
+    numbers[_key4] = arguments[_key4];
+  }
+  // [ [n, qtd], [n, qtd], [n, qtd] ]
+  var quantities = numbers.map(function (num) {
+    return [num, numbers.filter(function (n) {
+      return num === n;
+    }).length];
+  });
+  quantities.sort(function (a, b) {
+    return b[1] - a[1];
+  });
+  return quantities[0][0];
+};
+console.log("Moda: ".concat(mode(1, 1, 5, 4, 9, 7, 4, 3, 5, 2, 4, 0, 4)));
